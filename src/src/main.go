15b7362dc79b89fb6a7d476fa0bee07e32607f67
Marlon Bastida <marlon.bastida@gmail.com>
2025-02-17 14:06:54 -0300

    alteracao geral

--- src/src/main.go
+++ src/src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo - V2")
+	fmt.Fprintf(w, "Aplicacao exemplo")
 }
 
 func main() {
