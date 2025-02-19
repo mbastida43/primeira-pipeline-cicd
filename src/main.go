d2a6dedbba7414cb8374c06efaeef60c816a538b
Marlon Bastida <marlon.bastida@gmail.com>
2025-02-18 23:37:17 -0300

    Pipeline CD

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao Exemplo - v3")
+	fmt.Fprintf(w, "Aplicacao exemplo - v3")
 }
 
 func main() {
